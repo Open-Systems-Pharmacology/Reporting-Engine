123df69c0abaa0211cdf2e99fdfdd11f687c3cce
Juri Solodenko <Yuri05@users.noreply.github.com>
2017-08-24 16:27:16 +0200

    Fixes #17 Adjust installation test script (#19)
    Fixes #18 Use latest version of SimModel

--- src/testInstallation/testReportingEngineInstallation.m
+++ src/testInstallation/testReportingEngineInstallation.m
@@ -2,17 +2,11 @@ function isValid = testReportingEngineInstallation(rootFolderReportingEngine)
 
 cd(fullfile(rootFolderReportingEngine,'testInstallation'))
 
-% addpathes
-addpath '..\src\code\engine_library';
-addpath '..\src\matlab_toolbox\code';
-addpath '..\src\matlab_toolbox\code\auxiliaries';
-
 % name of xml
 xml = 'Children_OralSingle_IV_Multi.xml';
 
-% get defalt Setting
-Settings = getDefaultWorkflowSettings('meanModel','default');
-
-[~,isValid] = getApplicationProtocollFromXML(Settings,xml);
+%init and process simulation
+initSimulation(xml, []);
+isValid=processSimulation;
 
 return
