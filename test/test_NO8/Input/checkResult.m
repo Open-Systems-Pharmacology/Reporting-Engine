6e210f9d950c35dcc5269d2bd1e5f9e612fcdb92
Juri Solodenko <Yuri05@users.noreply.github.com>
2019-02-21 14:08:12 +0100

    Fixes #65 extend examples (#67)
    * deleted old tests
    
    * added tests
    
    * renamed
    
    * adjusted OSPLIb tests and master test script
    
    * runAllTests.m adjusted
    
    * runAllTests.m adjusted

--- test/test_NO8/Input/checkResult.m
+++ test/test_NO8/Input/checkResult.m
@@ -7,13 +7,13 @@ try
    
     
     % check if xml file was loaded and interpreted correctly
-    if ~exist('tmp/SingleIvBolus/applicationProtocol.mat','file')
+    if ~exist(fullfile('tmp','SingleIvBolus','applicationProtocol.mat'),'file')
         success = false;
         disp('failed in loading xml simulation file')
         return
     end
     
-    load tmp/SingleIvBolus/applicationProtocol.mat
+    load(fullfile('tmp','SingleIvBolus','applicationProtocol.mat'));
     if isempty(ApplicationProtocol)
         success = false;
         disp('failed in loading xml simulation file')
@@ -21,26 +21,20 @@ try
     end
     
     % cehck if population file was loaded correctly
-     if ~exist('tmp/SingleIvBolus/pop.mat','file')
-         success = false;
+    if ~exist(fullfile('tmp','SingleIvBolus','pop.mat'),'file')
+        success = false;
         disp('failed in loading pop csv')
-        return
-     end
-     
-     load 'tmp\SingleIvBolus\pop.mat';     
-    if length(parPaths)~=100
-         success = false;
-        disp('failed in loading pop csv, length parPaths not ok')
         return
     end
     
-    if length(parPaths)~=100
-         success = false;
+    load(fullfile('tmp','SingleIvBolus','pop.mat'));
+    if length(parPaths)~=104
+        success = false;
         disp('failed in loading pop csv, length parPaths not ok')
         return
     end
 
-    if abs(mean(mean(parValues))-47.1297)>0.01
+    if abs(mean(mean(parValues(:,5:end)))-48.0348)>0.01
          success = false;
         disp('failed in loading pop csv, mean of parValues not ok')
         return
