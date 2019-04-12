4b681c88b86556f6a984fed4deb42632650fb568
Unknown <chelle.pierre@gmail.com>
2019-04-12 11:37:50 -0400

    Update runQualificationWorkflow
    PlotSettings, TimeProfile and GOFMerged as treated tasks

--- src/code/qualification/runQualificationWorkflow.m
+++ src/code/qualification/runQualificationWorkflow.m
@@ -42,11 +42,11 @@ for i=1:length(TaskList)
             
             % Plot the results
             plotQualificationTimeProfile(WSettings, j, SimResult, ObservedDataSets, TimeProfile.Plot.Curves, TimeProfile.Plot.Axes, PlotSettings);
-            
+            % Pause option for debugging
+            % pause()
             SectionPath = getSection(ConfigurationPlan.Sections, TimeProfile.SectionId);
-            saveas(gcf,fullfile(SectionPath, ['OutputTimeProfile' num2str(j)]), 'png');
+            saveas(gcf,fullfile(SectionPath, ['OutputTimeProfile' num2str(j)]), 'pdf');
             close;
-            disp(j);
         end
     end
     
@@ -54,33 +54,63 @@ for i=1:length(TaskList)
     % Plot GOFMerged
     % Caution, function not finished, to be completed
     % So far save empty plots
-    if strcmp(TaskList{i}, 'GOFMerged')
+    if strcmp(TaskList{i}, 'GOFMergedPlots')
+        
         for j=1:length(ConfigurationPlan.Plots.GOFMergedPlots)
             
             GOFMerged=ConfigurationPlan.Plots.GOFMergedPlots(j);
             
             % Update plot settings if necessary (to be performed)
             % Currently not handled
-            WSettings = getDefaultWorkflowSettings('Qualification',''); close;
+            WSettings = getDefaultWorkflowSettings('Qualification','default'); close;
             
-            ax = getReportFigure(WSettings,1,1,[],'figureformat','landscape');
+            for k=1:length(GOFMerged)
+                
+                % Update plot settings if necessary (to be performed)
+                % Currently not handled
+                if isfield(GOFMerged(j), 'PlotSettings')
+                    nPlotSettings=GOFMerged(j).PlotSettings;
+                else
+                    nPlotSettings=PlotSettings;
+                end
+                
+                % Get all the groups for one GOF merged plot
+                Groups = GOFMerged.Groups;
+                
+                % Plot the Goodness of fit and output GMFE
+                GOFResults=plotQualificationGOFMerged(WSettings,j,Groups,ObservedDataSets,ConfigurationPlan.SimulationMappings, nPlotSettings);
+                
+                % Pause option for debugging
+                % pause()
+                
+                % Check plot type to perform predictedVsObserved, residualsOverTime or both
+                if ~isempty(strfind(GOFMerged.PlotType, 'residualsOverTime'))
+                    SectionPath = getSection(ConfigurationPlan.Sections, GOFMerged.SectionId);
+                    saveas(gcf,fullfile(SectionPath, ['OutputGOFMergedResiduals' num2str(k)]), 'png');
+                    close(gcf);
+                end
+                if ~isempty(strfind(GOFMerged.PlotType, 'predictedVsObserved'))
+                    SectionPath = getSection(ConfigurationPlan.Sections, GOFMerged.SectionId);
+                    saveas(gcf,fullfile(SectionPath, ['OutputGOFMergedPredvsObs' num2str(k)]), 'png');
+                    close(gcf);
+                end
+            end
             
-            SectionPath = getSection(ConfigurationPlan.Sections, GOFMerged.SectionId);
-            saveas(gcf,fullfile(SectionPath, ['OutputGOFMerged' num2str(j)]), 'png');
-            close;
         end
     end
     
     % Plot Comparison of Time profiles
+    % TO BE IMPLEMENTED
     if strcmp(TaskList{i}, 'ComparisonTimeProfilePlots')
+        %{
         for j=1:length(ConfigurationPlan.Plots.ComparisonTimeProfilePlots)
             
             ComparisonTimeProfile=ConfigurationPlan.Plots.ComparisonTimeProfilePlots(j);
             
-%            for k=1:length(ComparisonTimeProfile.OutputMappings)
-%                csvSimFile = getSimFile(TimeProfile, ComparisonTimeProfile.OutputMappings(k));
-%                SimResult(k) = loadSimResultcsv(csvSimFile, ComparisonTimeProfile.OutputMappings(k).Simulation);
-%            end
+            %            for k=1:length(ComparisonTimeProfile.OutputMappings)
+            %                csvSimFile = getSimFile(TimeProfile, ComparisonTimeProfile.OutputMappings(k));
+            %                SimResult(k) = loadSimResultcsv(csvSimFile, ComparisonTimeProfile.OutputMappings(k).Simulation);
+            %            end
             
             % Update plot settings if necessary (to be performed)
             % Currently not handled
@@ -97,6 +127,7 @@ for i=1:length(TaskList)
             saveas(gcf,fullfile(SectionPath, ['ComparisonTimeProfilePlots' num2str(j)]), 'png');
             close;
         end
-        
+        %}
     end
+    
 end
