b9e476abe6f6c76c5f585e4f7ffd05b67ae69a4f
Unknown <chelle.pierre@gmail.com>
2019-05-15 17:49:00 -0400

    Fixes #134 Refactor Qualification Workflow

--- src/code/qualification/QualificationWorkflow.m
+++ src/code/qualification/QualificationWorkflow.m
@@ -2,83 +2,25 @@
 % Qualification Plan Workflow developed with Matlab 2017b
 % --------------------------------------------------------------
 
-clear
+clear all
 close all
 
-% Set the working directy and create the output repository
-% This part can be remove or replaced on later versions PKRatio advanced_01
-% minimal PopulationTimeProfile
-REInput_path = 'QualificationPlanTests\examples\advanced_01\reporting engine input';
-cd(REInput_path)
+% List of examples to be tested 
+REInput_path = 'QualificationPlanTests\examples\minimal\reporting engine input';
+% REInput_path = 'QualificationPlanTests\examples\advanced_01\reporting engine input';
+% REInput_path = 'QualificationPlanTests\examples\PKRatio\reporting engine input';
+% REInput_path = 'QualificationPlanTests\examples\PopulationTimeProfile\reporting engine input';
+% REInput_path = 'QualificationPlanTests\examples\advanced_01\reporting engine input';
+
 REOutput_path = '..\reporting engine output';
-mkdir(REOutput_path);
+
+cd(REInput_path)
 
 % --------------------------------------------------------------
 % Get the Configuration Plan Settings
 jsonFile = 'report-configuration-plan.json';
-ConfigurationPlan = getConfigurationPlan(jsonFile);
 
-% Linearize the Sections input and create the folder structure
-ConfigurationPlan.Sections = generateOutputFolders(ConfigurationPlan.Sections, REOutput_path);
-
-% --------------------------------------------------------------
-% Copy Intro and Input content into right section
-if isfield(ConfigurationPlan, 'Intro')
-    for i=1:length(ConfigurationPlan.Intro)
-        [SectionPath, indexed_item] = getSection(ConfigurationPlan.Sections, ConfigurationPlan.Inputs(i).SectionId);
-        try
-            copyfile(ConfigurationPlan.Intro(i).Path, fullfile(REOutput_path));
-        catch exception
-            writeToReportLog('ERROR', sprintf('Input %d could not be copied: \n %s', num2str(i), exception.message), 'true', exception);
-            rethrow(exception);
-        end
-    end
-end
-for i=1:length(ConfigurationPlan.Inputs)
-    [SectionPath, indexed_item] = getSection(ConfigurationPlan.Sections, ConfigurationPlan.Inputs(i).SectionId);
-    try
-        copyfile(ConfigurationPlan.Inputs(i).Path, SectionPath);
-    catch exception
-        writeToReportLog('ERROR', sprintf('Input %d could not be copied: \n %s', num2str(i), exception.message), 'true', exception);
-        rethrow(exception);
-    end
-end
-
-% --------------------------------------------------------------
-% Global settings
-% Not developed yet: uses getDefaultWorkflowSettings from Reporting Engine
-WSettings = getDefaultWorkflowSettings('Qualification','default'); close;
-
-% --------------------------------------------------------------
-% Load the Observations Data Sets from csv into structures
-% Convert the input into cell array
-if ~iscell(ConfigurationPlan.ObservedDataSets)
-    ConfigurationPlan.ObservedDataSets=num2cell(ConfigurationPlan.ObservedDataSets);
-end
-if isempty(ConfigurationPlan.ObservedDataSets)
-    % If empty input, output ObservedDataSets is an empty variable
-    ObservedDataSets=[];
-else
-    for i=1:length(ConfigurationPlan.ObservedDataSets)
-        % For DDI and PK Ratio plots, Observed Data Type is different
-        % ObservedDataSets format to be determined
-        % Temporary format: structure where the observed data is a table y
-        if isfield(ConfigurationPlan.ObservedDataSets{i}, 'Type')
-            ObsTable = readtable(ConfigurationPlan.ObservedDataSets{i}.Path, 'Encoding', 'UTF-8');
-            ObservedDataSets(i).Id=ConfigurationPlan.ObservedDataSets{i}.Id;
-            ObservedDataSets(i).Type=ConfigurationPlan.ObservedDataSets{i}.Type;
-            ObservedDataSets(i).name=ConfigurationPlan.ObservedDataSets{i}.Path;
-            ObservedDataSets(i).y=ObsTable;
-        else
-            ObservedDataSets(i) = loadObservationscsv(ConfigurationPlan.ObservedDataSets{i}.Path,...
-                ConfigurationPlan.ObservedDataSets{i}.Id);
-        end
-    end
-end
-
-% --------------------------------------------------------------
-% Task list: fields in ConfigurationPlan.Plots
-TaskList = fields(ConfigurationPlan.Plots);
+[WSettings, ConfigurationPlan, TaskList, ObservedDataSets] = initializeQualificationWorkflow(jsonFile, REOutput_path);
 
 % --------------------------------------------------------------
 % run the Worklfow tasklist of ConfigurationPlan
