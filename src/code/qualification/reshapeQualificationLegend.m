b1911d7636c2ad42447968d1274cf7fbba4ee449
Unknown <chelle.pierre@gmail.com>
2019-07-22 08:55:10 -0400

    Hide plot behind legend to print legend only once reshaped

--- src/code/qualification/reshapeQualificationLegend.m
+++ src/code/qualification/reshapeQualificationLegend.m
@@ -1,49 +1,69 @@
 function lgd = reshapeQualificationLegend(lgd)
-% Algorithm that check legend size of current figure and reshape if necessary
-
-% Minimum FontSize allowed
-MinFontSize = 6;
-
-% Maximum number of characters allowed with minimum FontSize
-LegendLengthMax = 100;
+% Algorithm that break lines if is too long
 
 % Get figure position as [x0 y0 width height]
 figPosition = get(gca, 'Position');
 
-% If Legend caption is bigger than 10% of figure width
-% Legend Fontisize is decreased until minimum allowed FontSize
-while lgd.Position(3) >= 1.10*figPosition(3) && lgd.FontSize >= MinFontSize
-    lgd.FontSize = lgd.FontSize-0.5;
-end
-
-% Update figure position
-figPosition = get(gca, 'Position');
-    
-% If Legend caption is bigger than 60% of plot height
-% Legend Fontisize is decreased until minimum allowed FontSize
-while lgd.Position(4) >= 0.6*figPosition(4) && lgd.FontSize >= MinFontSize
-    lgd.FontSize = lgd.FontSize-0.5;
-end
-
-% If Minimum of FontSize is reached and the caption is still too wide
-% Split line in 2 
+% If caption is too wide split line in half 
 LegendLengths = cellfun(@(x)size(x, 2), lgd.String);
-if lgd.Position(3) >= 1.10*figPosition(3)
-    for LegendIndex = 1:length(lgd.String)
-        if LegendLengths(LegendIndex) > LegendLengthMax
-            % Split legend entry
-            splitLegend = strsplit(lgd.String{LegendIndex});
-            % Break line of the legend
-            strjoin([splitLegend(1:ceil(length(splitLegend)/2)), ...
-                newline , splitLegend(ceil(length(splitLegend)/2+1:end))]);
-        end
+% Get the number of line breaks:
+WidthRatio = lgd.Position(3)/figPosition(3);
+
+% Get the legend length ratio compared to Figure Width
+% Add an offset of 10% to ensure that all the legend is visible
+LegendLengthsRatios = ceil(WidthRatio.*LegendLengths./max(LegendLengths)+0.10);
+
+for LegendIndex = 1:length(lgd.String)
+    switch LegendLengthsRatios(LegendIndex)
+        case 2
+            % Since character '-' was often use to split different
+            % information, it was used as splitting character
+            LegendSplit = strsplit(lgd.String{LegendIndex}, '-');
+            if length(LegendSplit)<2
+                lgd.String{LegendIndex} = [lgd.String{LegendIndex}(1:ceil(LegendLengths(LegendIndex)/2)) ...
+                newline LegendSplit(ceil(LegendLengths(LegendIndex)/2)+1:end)];
+            else
+                lgd.String{LegendIndex} = strjoin([LegendSplit(1:ceil(length(LegendSplit)/2)) ...
+                strjoin([newline LegendSplit(ceil(length(LegendSplit)/2)+1:end)],'')], '-');
+            end
+        case 3
+            LegendSplit = strsplit(lgd.String{LegendIndex}, '-');
+            if length(LegendSplit)<3
+                lgd.String{LegendIndex} = [LegendSplit(1:ceil(LegendLengths(LegendIndex)/3)) ...
+                newline LegendSplit(ceil(LegendLengths(LegendIndex)/3)+1:ceil(2*LegendLengths(LegendIndex)/3)) ...
+                newline LegendSplit(ceil(2*LegendLengths(LegendIndex)/3)+1:end)];
+            else
+                lgd.String{LegendIndex} = strjoin([LegendSplit(1:ceil(length(LegendSplit)/3)) ...
+                strjoin([newline LegendSplit(ceil(length(LegendSplit)/3)+1:ceil(2*length(LegendSplit)/3))],'') ...
+                strjoin([newline LegendSplit(ceil(2*length(LegendSplit)/3)+1:end)]),''], '-');
+            end
+        case 4
+            LegendSplit = strsplit(lgd.String{LegendIndex}, '-');
+            if length(LegendSplit)<4
+                lgd.String{LegendIndex} = [LegendSplit(1:ceil(LegendLengths(LegendIndex)/4)) ...
+                strjoin([newline LegendSplit(ceil(LegendLengths(LegendIndex)/4)+1:ceil(2*LegendLengths(LegendIndex)/4))]) ...
+                strjoin([newline LegendSplit(ceil(2*LegendLengths(LegendIndex)/4)+1:ceil(3*LegendLengths(LegendIndex)/4))]) ...
+                strjoin([newline LegendSplit(ceil(3*LegendLengths(LegendIndex)/4)+1:end)])];
+            else
+                lgd.String{LegendIndex} = strjoin([LegendSplit(1:ceil(length(LegendSplit)/4)) ...
+                strjoin([newline LegendSplit(ceil(length(LegendSplit)/4)+1:ceil(2*length(LegendSplit)/4))],'') ...
+                strjoin([newline LegendSplit(ceil(2*length(LegendSplit)/4)+1:ceil(3*length(LegendSplit)/4))],'') ...
+                strjoin([newline LegendSplit(ceil(3*length(LegendSplit)/4)+1:end)],'')], '-');
+            end
     end
 end
 
-% If Minimum of FontSize is reached and the caption still shrinks the plot
-% Set legend within the plot
-% Update figure position
-figPosition = get(gca, 'Position');
-if lgd.Position(4) >= 0.6*figPosition(4)
-    lgd.Location = 'northwest';
+lgd.Location = 'South';
+set(gca, 'Position', get(lgd, 'Position'));
+
+CurvesProperties = get(gca,'children');          % Get the curves properties
+AnnotationTags = findall(gcf,'Type', 'Textbox'); % Get the Watermarks and other annotations
+
+for AnnotationIndex=1:length(AnnotationTags)
+    set(AnnotationTags(AnnotationIndex), 'visible', 'off');
 end
+
+for CurveIndex=1:length(CurvesProperties)
+    CurvesProperties(CurveIndex).XData = NaN*CurvesProperties(CurveIndex).XData;
+end
+axis off;
