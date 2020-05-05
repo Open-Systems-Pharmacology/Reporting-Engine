c3eed69acf84d5005d85805efc55f63face6eed1
sfrechen <sebastian.frechen@bayer.com>
2020-05-05 12:38:27 +0200

    extend concept of automatic line breaks for more than 2 lines

--- src/code/qualification/addLegendWithLineBreaks.m
+++ src/code/qualification/addLegendWithLineBreaks.m
@@ -4,28 +4,45 @@ set(currentFig, 'unit', 'inches');
 h_legend = legend(curvesHandle, cellfun(@(x) sprintf(x),curvesLegend,'UniformOutput',false));
 set(h_legend, 'unit', 'inches');
 nCharPerLine = max(cellfun(@(x) length(x),curvesLegend));
+nCharPerLineMax = nCharPerLine;
+maxNoLines = 2;
 while 1.2*h_legend.Position(3) > currentFig.Position(3)
     nCharPerLine = nCharPerLine-1;
-    for ll = 1:size(curvesLegend,2)
-        curvesLegend_lineBreaks{ll} = insertLineBreaks(curvesLegend{ll},nCharPerLine);
+    if nCharPerLine >= (nCharPerLineMax/maxNoLines)
+        for ll = 1:size(curvesLegend,2)
+            curvesLegend_lineBreaks{ll} = insertLineBreaks(curvesLegend{ll},nCharPerLine,maxNoLines);
+        end
+        h_legend = legend(curvesHandle, cellfun(@(x) sprintf(x),curvesLegend_lineBreaks,'UniformOutput',false));
+    else
+        nCharPerLine = nCharPerLineMax;
+        maxNoLines = maxNoLines + 1;
     end
-    h_legend = legend(curvesHandle, cellfun(@(x) sprintf(x),curvesLegend_lineBreaks,'UniformOutput',false));
 end
 
-function res = insertLineBreaks(str, NoCharPerLine)
-
-if size(str,2) > NoCharPerLine
-    II1=strfind(str,' ');
-    II2=strfind(str,'-');
-    II = sort([II1 II2]);
-    for ii = 0:(length(II)-1)
-        if size(str(1:II(end-ii)),2) <= NoCharPerLine
-            str1 = str(1:II(end-ii));
-            str2 = str((II(end-ii)+1):end);
-            res = sprintf('%s%s%s',str1,'\n',str2);
-            break
+function res = insertLineBreaks(str, NoCharPerLine, maxNoLines)
+residStr = str;
+res = '';
+for ll = 1:maxNoLines
+    if length(residStr) > NoCharPerLine && ll < maxNoLines
+        II1=strfind(residStr,' ');
+        II2=strfind(residStr,'-');
+        II = sort([II1 II2]);
+        if ~isempty(II)
+            for ii = 0:(length(II)-1)
+                if size(residStr(1:II(end-ii)),2) <= NoCharPerLine
+                    str1 = residStr(1:II(end-ii));
+                    residStr = residStr((II(end-ii)+1):end);
+                    res = sprintf('%s%s%s',res,str1,'\n');
+                    break
+                end
+            end
+        else
+            str1 = residStr(1:floor(NoCharPerLine));
+            residStr = residStr((ceil(NoCharPerLine)+1):end);
+            res = sprintf('%s%s%s',res,str1,'\n');
         end
+    else
+        res = sprintf('%s%s',res,residStr);
+        break
     end
-else
-    res = str;
 end
